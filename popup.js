6ce7071c6103a27b6f836168e16f108203fb766b
amueller8 <68559641+amueller8@users.noreply.github.com>
2020-08-08 15:21:51 -0400

    abby- working on creating new todos (jquery added)
    Added a jquery file to allow for user-side interactivity (creating new todo elements). Added that file to the manifest, as well as added a new add button/text field. Right now things are still a little broken, haha

--- popup.js
+++ popup.js
@@ -1,13 +1,46 @@
+$(document).ready(function(){
+  
+    $( ".widget input[type=submit]").button();
+    $('#plus').click(function(){
+    /*makes a new todo. i want to do this without append-mit seems sloppy to me
+    or like there is a more efficient way to do this) but i'm too lazy rn. eventually...
+    in the future i will want to exchange the general id for the id
+    of the category that the user is in 
+    */
+        //if $('#task-to-do'.val()){
+        var new_task = '#task-to-do'.val();
+
+        $('#general').append("<dd><input class = 'task' type = 'checkbox'/>"+ new_task+ "</input></dd>");
+      
+   
+    });
+    
+    
+    /* currently not being used
+    $('.minus').click(function(){
+      //deletes EVERYTHING. :D
+      var k = alert("Are you sure you want to clear your todo list?");
+      //^checks to make sure you are okay, though idk if u really have a choice.
+      if($('.task').has('.chk')){
+        if($('.chk').is(":checked")){
+          //makes sure you've at least done something before they all go bye-bye. without being saved. :D
+           $('.td_item').remove();
+        } else{
+          //lets you know that you've been slacking. how polite
+          var niceTry = alert("You don't seem to have done anything on your list.");
+        }
+      }
+     
+      
+      
+      
+    });*/
+  
+    
+  });
+
 let tasks = document.getElementsByClassName('task');
 
 for (var i = 0; i < tasks.length; i++) {
-    tasks[i].addEventListener("click", function (result) {
-        console.log(result.target.checked);
-        chrome.storage.sync.set({ i : result.target.checked }, function () {
-            console.log(i + result.target.checked + " Storage");
-        });
-    });
-    chrome.storage.sync.get([i], function (result) {
-        console.log('Value currently is ' + result);
-    });
+  
 }

e5f4336426b7837756c9391cd92cb4518ef77e34
unknown <mintychocolate101@gmail.com>
2020-08-08 14:18:34 -0700

    Formatting

--- popup.js
+++ popup.js
@@ -45,11 +45,14 @@ let sections = document.getElementsByClassName('section-header');
 for (var i = 0; i < sections.length; i++) {
     sections[i].addEventListener("click", function () {
         var content = this.nextElementSibling;
-        console.log("content: " + content.textContent);
         if (content.style.display === "block") {
             content.style.display = "none";
+            this.textContent = this.textContent.replace('-', '+');
+            console.log(this.innerHTML);
         } else {
             content.style.display = "block";
+            this.textContent = this.textContent.replace('+', '-');
+            console.log(this.innerHTML);
         }
     });
 }

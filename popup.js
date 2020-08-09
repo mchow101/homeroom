2bf8d44eef758e2b9d837bd0fb4d9393813288ae
Shruthik Musukula <shruthik.musukula@gmail.com>
2020-08-09 12:57:32 -0700

    added minute timer logic

--- popup.js
+++ popup.js
@@ -28,4 +28,193 @@ function changetab() {
     }
 }
 
+$(document).ready(function () {
+    changetab();
 
+    let tasks = document.getElementsByClassName('task');
+    let sections = document.getElementsByClassName('section-header');
+
+    let classes = document.getElementsByClassName('classes');
+    let classSections = document.getElementsByClassName('class-section-header');
+
+    for (var i = 0; i < sections.length; i++) {
+        sections[i].nextElementSibling.innerHTML = sections[i].nextElementSibling.innerHTML + '<dd><input type="text" id="' + sections[i].textContent.substring(1) + '" class="new-todo"></input></dd>';
+        sections[i].id = sections[i].textContent.substring(1);
+        sections[i].addEventListener("click", function () {
+            var content = this.nextElementSibling;
+            if (content.style.display === "block") {
+                content.style.display = "none";
+                this.textContent = this.textContent.replace('-', '+');
+            } else {
+                content.style.display = "block";
+                this.textContent = this.textContent.replace('+', '-');
+            }
+        });
+    }
+
+    // copied above and tried to tweak for classes
+    for (var i = 0; i < classSections.length; i++) {
+        classSections[i].nextElementSibling.innerHTML = classSections[i].nextElementSibling.innerHTML + '<dd><input type="text" id="' + classSections[i].textContent.substring(1) + '" class="new-class"></input></dd>';
+        classSections[i].id = classSections[i].textContent.substring(1);
+        classSections[i].addEventListener("click", function () {
+            var content = this.nextElementSibling;
+            if (content.style.display === "block") {
+                content.style.display = "none";
+                this.textContent = this.textContent.replace('-', '+');
+            } else {
+                content.style.display = "block";
+                this.textContent = this.textContent.replace('+', '-');
+            }
+        });
+    }
+
+    // this triggered when the add button existed
+    // we can probably delete soon haha
+    /*
+    $('.plus').click(function () {
+        var content = $(this).next().val()
+        if (content != "") {
+            console.log(this.id);
+            $(this).before('<label><input type="checkbox" class="task"></input><span>' + content + '</span></label><br>');
+        }
+    });
+    */
+
+    $('.new-todo').focus(function () {
+        $(this).keypress(function (event) {
+            if (event.which == 13) {
+                var content = $(this).val();
+                if (content != "") {
+                    console.log(this.id)
+                    $(this).before('<label><input type="checkbox" class="task"></input><span>' + content + '</span></label><br>');
+                    $(this).val("");
+                }
+
+            }
+
+        });
+
+
+
+    });
+
+    // Pomodoro Timer Code is Below
+    let progressBar = document.querySelector('.e-c-progress');
+    let indicator = document.getElementById('e-indicator');
+    let pointer = document.getElementById('e-pointer');
+    let length = Math.PI * 2 * 100;
+
+    progressBar.style.strokeDasharray = length;
+
+    function update(value, timePercent) {
+        var offset = - length - length * value / (timePercent);
+        progressBar.style.strokeDashoffset = offset;
+        pointer.style.transform = `rotate(${360 * value / (timePercent)}deg)`;
+    };
+
+    //circle ends
+    const displayOutput = document.querySelector('.display-remain-time')
+    const pauseBtn = document.getElementById('pause');
+    const setterBtns = document.querySelectorAll('button[data-setter]');
+    const workInput = document.getElementById('work-period');
+
+    let intervalTimer;
+    let timeLeft;
+    //let wholeTime = 25;
+
+    let workTime = document.getElementById('work-period').value * 60; // manage this to set the whole time 
+    //let breakTime = document.getElementById('break-period').value * 60;
+
+    let isPaused = false;
+    let isStarted = false;
+
+    update(workTime, workTime); //refreshes progress bar
+    displayTimeLeft(workTime);
+
+    function changeWholeTime(seconds) {
+        if ((workTime + seconds) > 0) {
+          workTime += seconds;
+            update(workTime, workTime);
+        }
+    }
+
+    /*for (var i = 0; i < setterBtns.length; i++) {
+        setterBtns[i].addEventListener("click", function (event) {
+            var param = this.dataset.setter;
+            switch (param) {
+                case 'minutes-plus':
+                    changeWholeTime(1 * 60);
+                    break;
+                case 'minutes-minus':
+                    changeWholeTime(-1 * 60);
+                    break;
+                case 'seconds-plus':
+                    changeWholeTime(1);
+                    break;
+                case 'seconds-minus':
+                    changeWholeTime(-1);
+                    break;
+            }
+            displayTimeLeft(wholeTime);
+        });
+    }*/
+
+    function timer(seconds) { //counts time, takes seconds
+        let remainTime = Date.now() + (seconds * 1000);
+        displayTimeLeft(seconds);
+
+        intervalTimer = setInterval(function () {
+            timeLeft = Math.round((remainTime - Date.now()) / 1000);
+            if (timeLeft < 0) {
+                clearInterval(intervalTimer);
+                isStarted = false;
+                setterBtns.forEach(function (btn) {
+                    btn.disabled = false;
+                    btn.style.opacity = 1;
+                });
+                displayTimeLeft(workTime);
+                pauseBtn.classList.remove('pause');
+                pauseBtn.classList.add('play');
+                return;
+            }
+            displayTimeLeft(timeLeft);
+        }, 1000);
+    }
+
+    function pauseTimer(event) {
+        if (isStarted === false) {
+            timer(workTime);
+            isStarted = true;
+            this.classList.remove('play');
+            this.classList.add('pause');
+
+            setterBtns.forEach(function (btn) {
+                btn.disabled = true;
+                btn.style.opacity = 0.5;
+            });
+
+        } else if (isPaused) {
+            this.classList.remove('play');
+            this.classList.add('pause');
+            timer(timeLeft);
+            isPaused = isPaused ? false : true
+        } else {
+            this.classList.remove('pause');
+            this.classList.add('play');
+            clearInterval(intervalTimer);
+            isPaused = isPaused ? false : true;
+        }
+    }
+
+    function displayTimeLeft(timeLeft) { //displays time on the input
+        let minutes = Math.floor(timeLeft / 60);
+        let seconds = timeLeft % 60;
+        let displayString = `${minutes < 10 ? '0' : ''}${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+        displayOutput.textContent = displayString;
+        update(timeLeft, workTime);
+    }
+
+    pauseBtn.addEventListener('click', pauseTimer);
+
+    workInput.value.addEventListener('change', displayTimeLeft(document.getElementById("work-period").value));
+});

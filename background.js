7a3c83f39178e175ede6023656a6055afa2986fd
Shruthik Musukula <shruthik.musukula@gmail.com>
2020-08-09 19:30:12 -0700

    merged with oauth and sending db info

--- background.js
+++ background.js
@@ -12,3 +12,45 @@ chrome.runtime.onInstalled.addListener(function () {
     });
 
 });
+
+chrome.identity.getAuthToken({ interactive: true }, function (token) {
+    console.log(token);
+});
+
+chrome.identity.getProfileUserInfo(function (info) {
+  email = info.email;
+  id = info.id;
+  console.log(email);
+  console.log(id);
+});
+
+chrome.runtime.onInstalled.addListener(function (request) {
+  console.log("Getting message");
+  var settings = {
+    url: "https://us-central1-aiot-fit-xlab.cloudfunctions.net/gettasksbyuser",
+    type: "POST",
+    //timeout: 0,
+    success: function(data) {
+        console.log(data);
+    },
+    error: (err) => {
+        console.log(err.responseJSON);
+    },
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*"
+    },
+    data: {"uid": "1"},
+    dataType: 'json',
+    contentType: "application/json"
+  };
+
+  $.ajax(settings);
+  /*$.ajax({
+    url: 'https://en.wikipedia.org/w/api.php?' +
+         'action=opensearch&format=json&maxlag=5&search=' + encodeURIComponent("cuba"),
+    success(data) {
+        console.log(data);
+    },
+  });*/
+});

ac510ea3f5e422cea83a1c6ed1eb897027111f5b
unknown <mintychocolate101@gmail.com>
2020-08-10 12:33:54 -0700

    asdf

--- popup.js
+++ popup.js
@@ -110,8 +110,8 @@ $(document).ready(function () {
                 var content = $(this).val();
                 if (content != "") {
                     console.log(document.getElementById('class-list').innerHTML);
-                    $('#class-list').prepend('<h5 class="class-header featurette-heading"><span class="featurette-heading">+ </span>' + content + '</h5><div class="task-section lead"></div>');
-                    $('#task-list').prepend('<h5 class="section-header featurette-heading"><span class="featurette-heading">+ </span>' + content + '</h5><div class="task-section lead"></div>');
+                    $('#class-list').prepend('<h5 class="class-header"><span>+ </span>' + content + '</h5><div class="task-section lead"></div>');
+                    $('#task-list').prepend('<h5 class="section-header"><span>+ </span>' + content + '</h5><div class="task-section lead"></div>');
                     $(this).val("");
                     section_setup(document.getElementsByClassName("class-header")[0]);
                     add_todo_input();
